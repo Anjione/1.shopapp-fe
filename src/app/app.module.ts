b8f16cb894fe01223779859ed8e559e3c40c500f
Nguyễn Đăng An <annd162@viettel.com.vn>
2024-03-13 00:41:04 +0700

    update home, create order, order-detail, order-confirm, header, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+    DetailProductComponent,
+    OrderConfirmComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    // HomeComponent,
+    // OrderComponent,
+    OrderConfirmComponent
+  ]
 })
 export class AppModule { }
